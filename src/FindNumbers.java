74a728a6843d8114b5dc22092d46af1b7f6121f6
GrandmaPete <17cabernathy@greenwichcsd.org>
2016-12-08 20:09:58 -0500

    Just numbers
    Just messing around

---
+++ src/FindNumbers.java
@@ -0,0 +1,44 @@
+
+public class FindNumbers {
+
+	public static void main(String[] args) {
+//		int iVal = 100;
+//		int iVal2 = iVal;
+//		System.out.println(iVal);
+//		System.out.println(iVal2);
+//		iVal = 300;
+//		System.out.println(iVal);
+//		System.out.println(iVal2);
+		
+//		byte bVal = 100;
+//		long lVal = 123456789;
+//		
+//		lVal = bVal;
+//		bVal = (byte)lVal;
+//		
+//		float fVal1 = 13.0f;
+//		float fVal2 = 5;
+//		
+//		System.out.println(fVal1/fVal2);
+//		
+//		short sVal1 = 13;
+//		short sVal2 = 5;
+//		
+//		System.out.println(sVal1/sVal2);
+//		
+//		System.out.println(fVal1/sVal2);
+		
+		int newVal = 5;
+		
+		System.out.println(newVal++);
+		System.out.println(newVal);
+		System.out.println(++newVal);
+		System.out.println(newVal+37);
+		
+		System.out.println(newVal += 5);
+		System.out.println(newVal);
+		
+	}
+	
+	
+}
